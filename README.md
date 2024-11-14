4b356ca13315d772d2bee1d760a7a36faa9d7667
sravani <sravanimandela140@gmail.com>
2024-11-14 16:56:14 +0530

    iam role

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Deploying-Application-on-EKS-Cluster
+
+node - we can assume this as an ec2 so for this ec2 we need to attach an IAM role.(iamrole.tf)
+
--- iamrole.tf
+++ iamrole.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "eks_cluster_role" {
 
   assume_role_policy = jsonencode({
     "Version": "2012-10-17",
-    "Statement": [
+    "Statement": [  
       {
         "Effect": "Allow",
         "Principal": {
