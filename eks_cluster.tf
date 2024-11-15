d8f4c99cd56b8e4c5f52b8868dd2f447fad91d45
sravani <sravanimandela140@gmail.com>
2024-11-15 18:22:22 +0530

    modified files

--- eks_cluster.tf
+++ eks_cluster.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "my-eks-cluster"
-  role_arn = aws_iam_role.eks_node_role.arn
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids = aws_subnet.eks_subnet[*].id
---
+++ node_role.tf
@@ -0,0 +1,58 @@
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "eks_node_policy" {
+  name        = "eks-node-policy"
+  description = "EKS Node Role Permissions"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "eks:DescribeCluster",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "autoscaling:DescribeAutoScalingGroups",
+          "cloudwatch:PutMetricData",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams",
+          "iam:ListRolePolicies",
+          "iam:GetRolePolicy",
+          "iam:GetRole",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets"
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_role_policy_attach" {
+  policy_arn = aws_iam_policy.eks_node_policy.arn
+  role       = aws_iam_role.eks_node_role.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy_attachment" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
