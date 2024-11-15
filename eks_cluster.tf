a55eb4900d398b09136657d7bc7e357f375701e8
sravani <sravanimandela140@gmail.com>
2024-11-15 13:18:27 +0530

    eks cluster

---
+++ eks_cluster.tf
@@ -0,0 +1,12 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.eks_subnet[*].id
+  }
+
+  tags = {
+    Name = "my-eks-cluster"
+  }
+}
